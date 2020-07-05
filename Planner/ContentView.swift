e7e7d41b1e633cb5ddccb833c75bed98809272d1
Bryam Soto <bryam.soto@avantica.net>
2020-07-04 23:54:55 -0500

    Initial Commit

---
+++ Planner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by Bryam Soto Ñahui on 7/4/20.
+//  Copyright © 2020 Speira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
